d486bf7d00047cca498f956cadca06fd6b99279f
Isla <Islajiin@gmail.com>
2025-03-30 23:06:31 +0900

    [feat]-각 필드와 버튼들 action 이랑 outlet연결

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    @IBAction func login(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
